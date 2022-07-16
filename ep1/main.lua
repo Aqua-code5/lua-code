2e57844187367f82aa31b9296531245c458bc1cb
Aqua-code5 <aquagamingfish@gmail.com>
2022-07-16 13:05:34 -0500

    init

---
+++ ep1/main.lua
@@ -0,0 +1,5 @@
+local myName = "Rob"
+local favNumber = 21
+local isAlive = true
+
+print("Hell my name is " .. myName .. " my favorite number is " .. favNumber .. " and i am alive yes or no ")
---
+++ ep2/main.lua
@@ -0,0 +1,16 @@
+local a = 5
+local b = 10
+
+local addtion = 7 + b
+
+local subtraction = b - a
+
+local multiplaytion = a * b
+
+local dividison = b / a
+
+local power = 5^3
+
+local modulus = 6 % 13 
+
+print(modulus) 

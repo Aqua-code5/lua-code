43158920383ce60aef73c1cdd9309872aa8b184a
Aqua-code5 <aquagamingfish@gmail.com>
2022-07-16 13:11:38 -0500

    ep3 added

--- ep3/main.lua
+++ ep3/main.lua
@@ -1,18 +1,17 @@
 local hp = 65
 local playerName = "Aqua"
 
---if hp < 50 then
- --   print("Player is low on HP")
---else  
---    print("You are health")
---end
+if hp < 50 then
+   print("Player is low on HP")
+else  
+    print("You are health")
+end
 
---if playerName == "Rob" then
---    print("Player has a valid name!")
-
---elseif playerName == "Aqua" then
- --   print("Player has a valid name!")
---end
+if playerName == "Rob" then
+    print("Player has a valid name!")
+elseif playerName == "Aqua" then
+  print("Player has a valid name!")
+end
 
 if hp == 100 and playerName == "Aqua" then
     print("Player is valid!")
